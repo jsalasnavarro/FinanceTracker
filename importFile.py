cabe8106df316eaa4f2225e465e23f5cf943079d
jawn <jonsal.salas@gmail.com>
2021-11-04 22:53:15 -0600

    added tests for checking values when importing from google sheets, imported data from google sheets api, added individual expenses, and checked that in postgres DB is accurate

--- importFile.py
+++ importFile.py
@@ -13,22 +13,56 @@ class ImportFile:
 
 		# access sheets in spreadsheet object
 		sheets = ss.sheets
-		numSheets = len(ezsheets.listSpreadsheets())
-		
-		for sheet in range(numSheets):
-			for row in range(sheets[sheet].rowCount):
+
+		# run query for stored categories, table to be added
+		paymentTypes = ['afcc', 'discover', 'citi', 'paypal', 'cash']
+		category = []
+		for title in ss.sheetTitles:
+			for row in range(ss[title].rowCount):
 				newRow = []
-				for col in range(sheets[0].columnCount):
-					value = sheets[0][col + 1, row + 1]
+				for col in range(ss[title].columnCount):
+					value = ss[title][col + 1, row + 1]
 					# check for blank cells in sheet
-					if value != "":
+					if value != "" and value != None:
 						# skip header
 						if row  != 0:
-							# date column
-							if (col == 4):
-								# parse string to a datetime, format datetime type to string w/ 
-								# format for query
-								value = datetime.strftime(datetime.strptime(value, '%A, %B %d, %Y'), '%Y-%m-%d')
+
+							# # vendor
+							if col == 0:
+								value = value.lower()
+								
+							# cost
+							if col == 1:
+								# test if value can be converted to a float
+								try:
+									testValue = value.replace('$','')
+									testValue = testValue.replace(' ', '')
+									testValue = testValue.replace(',', '')
+									testValue = float(testValue)
+								except:
+									value = 00.00
+							# category
+							if col == 2:
+								value = value.lower()
+								if value not in category:
+									category.append(value)
+
+							# payment type
+							if col == 3:
+								value = value.lower()
+								if value not in paymentTypes:
+									paymentTypes.append(value)
+
+							# date
+							if col == 4:
+								# parse string to a datetime
+								try:
+									parse = datetime.strptime(value, '%A, %B %d, %Y')
+								
+								except:
+									parse = datetime.strptime('Monday, January 1, 1900', '%A, %B %d, %Y')
+								# format datetime type to string w/format for query
+								value = datetime.strftime(parse, '%Y-%m-%d')
 						
 						newRow.append(value)
 
--- scratch.py
+++ scratch.py
@@ -1,15 +1,80 @@
 from expense import Expense
 from importfile import ImportFile
+import ezsheets
+from datetime import datetime
 
-# expense, cost, details, payment, date, password)
-# expenses = [['test2', '100.00', 'rent', 'discover', '2021-10-31'], ['test3', '56.40', 'groceries', 'citi', None]]		
+# import file data from google spreadsheets file
+# newFile = ImportFile("Copy of Finances").googleSheets()
+# print(newFile)
 
-# newExpense = Expense('test1', 105.50, 'fun money', 'citi', '2021-10-11','northernlights')
-# newExpense.setExpense()
+# add expenses indidividually
+# vendor, cost, category, paymentType, date
+newExpense = Expense('Harmons', '$56.12','groceries', 'afcc', '2021-11-03').setExpense()
+newExpense = Expense('car repairs-fuel pressure regulator', '$61.41','autozone', 'afcc', '2021-11-03').setExpense()
+newExpense = Expense('Petco', '$7.53','Clifford-treats', 'afcc', '2021-11-03').setExpense()
+newExpense = Expense('ProRenter', '$1528','rent', 'citi', '2021-11-01').setExpense()
+newExpense = Expense('northface', '$59.27','remainder', 'citi', '2021-11-01').setExpense()
+newExpense = Expense('comcast', '$50.80','wifi', 'citi', '2021-11-01').setExpense()
 
-# for exp in expenses:
-# 	newExpense = Expense(exp[0], exp[1], exp[2], exp[3], exp[4])
-# 	newExpense.setExpense()
 
-newFile = ImportFile("Copy of Finances").googleSheets()
-print(newFile)
+# # access spreadsheet object
+# ss = ezsheets.Spreadsheet('Copy of Finances')
+
+# # access sheets in spreadsheet object
+# sheets = ss.sheets
+
+# for title in ss.sheetTitles:
+# 	print(title)
+# access spreadsheet object
+# ss = ezsheets.Spreadsheet("Copy of Finances")
+
+# # access sheets in spreadsheet object
+# sheets = ss.sheets
+
+# # run query for stored categories, table to be added
+# paymentTypes = ['afcc', 'discover', 'citi', 'paypal', 'cash']
+
+# for title in ss.sheetTitles:
+# 	for row in range(ss[title].rowCount):
+# 		newRow = []
+# 		for col in range(ss[title].columnCount):
+# 			value = ss[title][col + 1, row + 1]
+# 			# check for blank cells in sheet
+# 			if value != "" or not None:
+# 				# skip header
+# 				if row  != 0:
+# 					# vendor
+# 					if col == 0:
+# 						print(col, type(value))
+# 					# cost
+# 					if col == 1:
+# 						# test if value can be converted to a float
+# 						try:
+# 							testValue = value.replace('$','')
+# 							testValue = testValue.replace(' ', '')
+# 							testValue = testValue.replace(',', '')
+# 							testValue = float(testValue)
+# 						except:
+# 							value = 00.00
+# 					# category
+# 					if col == 2:
+# 						value = value.lower()
+# 						if value not in paymentTypes:
+# 							paymentTypes.append(value)
+
+# 					# payment type
+# 					if col == 3:
+# 						# check if payment type is new
+# 						print(col, type(value))
+# 					# date
+# 					if col == 4:
+# 						print(col, type(value))
+# 						# parse string to a datetime
+# 						try:
+# 							parse = datetime.strptime(value, '%A, %B %d, %Y')
+						
+# 						except:
+# 							parse = datetime.strptime('0000-00-00', '%A, %B %d, %Y')
+
+# 						# format datetime type to string w/format for query
+# 						value = datetime.strftime(parse, '%Y-%m-%d')
