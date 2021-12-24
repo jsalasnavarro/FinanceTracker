b47f5abf432b792f08776d0f2f3bbf06842b1bd4
jawn <jonsal.salas@gmail.com>
2021-12-24 09:13:21 -0700

    refactored code; added methods for select, update, and insert queries

--- runquery.py
+++ runquery.py
@@ -17,134 +17,156 @@ class RunQuery:
 		cursor.close()
 		connection.close()
 
+	def selectQuery(self, cursor, table, search, arguments, columns = "*"):
+		queryString = f"SELECT {columns} FROM {table} WHERE {search} = %s"
+		cursor.execute(queryString, arguments)
+
+		if columns == "*":
+			results = cursor.fetchall()
+		else:
+			results = cursor.fetchall()[0][0]
+
+		return results
+		
+	def insertQuery(self, cursor, table, columns, values, returnValue=""):
+		# make a tuple of %s placeholders that is same size as values list
+		placeArgs = str(tuple(["%s"]*len(values))).replace("'","")
+		insertQuery = f"INSERT INTO {table}{columns} VALUES {placeArgs} {returnValue}"
+		cursor.execute(insertQuery, values)
+
+		# only return results if returnValue is passed into function
+		if returnValue != "":
+			return cursor.fetchone()[0]
+
+	def updateQuery(self, cursor, table, setArg, whereArg, params):
+		loanQuery = f"UPDATE {table} SET {setArg} = %s WHERE {whereArg} = %s"
+		cursor.execute(loanQuery, params)
+
+
 	def insertExpense(self, source, cost, category, account, details, date):
 		cursor, connection = self.createConnection()
 
-		# query category and account
-		checkCategory = "SELECT * FROM expense_categories WHERE expense_category = %s"
-		cursor.execute(checkCategory, [category,])
-		categoryResults = cursor.fetchall()
-
-		checkAccount = "SELECT * FROM finance_accounts WHERE account = %s"
-		cursor.execute(checkAccount, [account,])
-		accountResults = cursor.fetchall()
+		# check if current expense category and account already exist by checking length of returned result
+		categoryResults = self.selectQuery(cursor, "expense_categories", "expense_category",[category,])
+		accountResults = self.selectQuery(cursor, "finance_accounts", "account", [account,])
 
 		# if category or account don't exist insert into tables, else get existing ids
 		if len(categoryResults) == 0:
-			insertCategory = "INSERT INTO expense_categories(expense_category) VALUES (%s) RETURNING category_id"
-			cursor.execute(insertCategory, [category,])
-			categoryId = cursor.fetchone()[0]
+			categoryId = self.insertQuery(cursor, "(expense_categories)", "expense_category", [category,], "RETURNING category_id")
 		else:
-			query = "SELECT category_id FROM expense_categories WHERE expense_category = %s"
-			cursor.execute(query, [category,])
-			categoryId = cursor.fetchall()[0][0]
+			categoryId = self.selectQuery(cursor, "expense_categories", "expense_category", [category,], "category_id")
 
 		if len(accountResults) == 0:
-			insertAccount = "INSERT INTO finance_accounts (account) VALUES (%s) RETURNING account_id"
-			cursor.execute(insertAccount, [account,])
-			accountId = cursor.fetchone()[0]
+			accountId = self.insertQuery(cursor, "finance_accounts", "(account)", [account,], "RETURNING account_id")
 		else:
-			query = "SELECT account_id FROM finance_accounts WHERE account = %s"
-			cursor.execute(query, [account,])
-			accountId = cursor.fetchall()[0][0]
+			accountId = self.selectQuery(cursor, "finance_accounts", "(account)", [account,], "account_id")
 
 		# insert expense into the table
-		expense = [source, cost, categoryId, accountId, details, date]
-		query = "INSERT INTO " + self.table + " (vendor, cost, category_id, account_id, details, dt) VALUES (%s,%s, %s, %s, %s, %s) \n RETURNING expense_id"
-		cursor.execute(query, expense)
-		expenseId = cursor.fetchone()[0]
+		expenseValues = [source, cost, categoryId, accountId, details, date]
+		expenseColumns = "(vendor, cost, category_id, account_id, details, dt)"
+		expenseId = self.insertQuery(cursor, "monthly_expenses", expenseColumns, expenseValues, "RETURNING expense_id")
 
 		# query account balance
-		query = "SELECT balance FROM finance_accounts WHERE account_id = %s"
-		cursor.execute(query, [accountId,])
-
-		# get balance as a string and remove '$'
-		balance = cursor.fetchall()[0][0].replace('$','')
+		balance = self.selectQuery(cursor, "finance_accounts", "account_id", [accountId,], "balance").replace('$','')		
+		# convert to a float if no apostrophe in balance
 		try:
 			balance = float(balance)
-		# else change to a foat
 		except:	
 			balance = float(balance.replace(',',''))
 
-		# update credit loan balances
+		# update loan balances
 		if category == 'mines loan' or category == 'america first car loan':
 			# dict for each account name and id
 			loanAccounts = {'mines loan': 13, 'america first car loan': 12}
 			loanId = loanAccounts[category]
 
 			# query balance of loan
-			loanQuery = "SELECT balance FROM finance_accounts WHERE account_id = %s"
-			cursor.execute(loanQuery, [loandId,])
-			loanBalance = cursor.fetchall()[0][0]
+			loanBalance = self.selectQuery(cursor, "finance_accounts", "account_id", [loanId,], "balance")
 
 			# calculate remaining balance after cost applied
 			if loanId == 13:
 				# interest paid
-				interest = balance * (12/0.45)
-				# principle paid
+				interest = balance * (12/0.045)
+				# principal paid
 				principle = cost - interest
 				newBalance = loanBalance - principle
 
 			elif loanId == 12:
 				newBalance = loanBalance - cost
 
-			# update account
-			loanQuery = "UPDATE finance_accounts SET balance = %s WHERE account_id = %s"
-			cursor.execute(loanQuery, [newBalance, loanId])
+			# update credit account with new balance
+			self.updateQuery(cursor, "finance_accounts", "balance", "account_id", [newBalance, loanId])
 
-			# update account history
-			query = "INSERT INTO finance_account_history (account_id, balance, dt, expense_id) VALUES (%s, %s, %s, %s)"
-			cursor.execute(query, [loandId, newBalance, date, expenseId])
-
+			# update account history with a record of new balance
+			historyValues = [loandId, newBalance, date, expenseId]
+			historyColumns = "(account_id, balance, dt, expense_id)"
+			self.insertQuery(cursor, "finance_account_history", historyColumns, historyValues)
 
 		# subtract cost from balance and insert into finance history table
 		newBalance = balance - cost
-		query = "INSERT INTO finance_account_history (account_id, balance, dt, expense_id) VALUES (%s, %s, %s, %s)"
-		cursor.execute(query, [accountId, newBalance, date, expenseId])
+		historyColumns = "(account_id, balance, dt, expense_id)"
+		historyValues = [accountId, newBalance, date, expenseId]
+		self.insertQuery(cursor, "finance_account_history", historyColumns, historyValues)
 
 		# update account balance
-		query = "UPDATE finance_accounts SET balance = %s WHERE account_id = %s"
-		cursor.execute(query, [newBalance, accountId])
+		self.updateQuery(cursor, "finance_accounts", "balance", "account_id", [newBalance, accountId])
 
 		self.closeConnection(cursor, connection)
 
 	def insertIncome(self, source, income, category, account, date):
 		cursor, connection = self.createConnection()
 
-		checkAccount = "SELECT * FROM finance_accounts WHERE account = %s"
-		cursor.execute(checkAccount, [account,])
-		accountResults = cursor.fetchall()
+		# checkAccount = "SELECT * FROM finance_accounts WHERE account = %s"
+		# cursor.execute(checkAccount, [account,])
+		# accountResults = cursor.fetchall()
+		# cursor, table, search, arguments, columns = "*"
+		accountResults = self.selectQuery(cursor, "finance_accounts", "account", [account,])
 
 		if accountResults == 0:
-			insertAccount = "INSERT INTO finance_accounts (account) VALUES (%s) RETURNING account_id"
-			cursor.execute(query, [account,])
-			accountId = cursor.fetchone()[0]
+			# cursor, table, columns, values, returnValue=""
+			# insertAccount = "INSERT INTO finance_accounts (account) VALUES (%s) RETURNING account_id"
+			# cursor.execute(query, [account,])
+			# accountId = cursor.fetchone()[0]
+			accountId = self.insertQuery(cursor, "finance_accounts", "(account)", [account,], "RETURNING account_id")
 		else:
-			query = "SELECT account_id FROM finance_accounts WHERE account = %s"
-			cursor.execute(query, [account,])
-			accountId = cursor.fetchall()[0][0]
+			# query = "SELECT account_id FROM finance_accounts WHERE account = %s"
+			# cursor.execute(query, [account,])
+			# accountId = cursor.fetchall()[0][0]
+			accountId = self.selectQuery(cursor, "finance_accounts", "account", [account,], "account_id")
 
 		incomeAdd = [source, income, category, accountId, date]
-		query = "INSERT INTO " + self.table + " (source, income, income_category, account_id, dt) VALUES (%s, %s, %s, %s, %s) RETURNING income_id"
-		cursor.execute(query, incomeAdd)
-		incomeId = cursor.fetchone()[0]
+		incomeColumns = "(source, income, income_category, account_id, dt)"
+		# query = "INSERT INTO " + self.table + " (source, income, income_category, account_id, dt) VALUES (%s, %s, %s, %s, %s) RETURNING income_id"
+		# cursor.execute(query, incomeAdd)
+		# incomeId = cursor.fetchone()[0]
+		incomeId = self.insertQuery(cursor, "monthly_income", incomeColumns, incomeAdd, "RETURNING income_id")
 
 		# query account balance
-		query = "SELECT balance FROM finance_accounts WHERE account_id = %s"
-		cursor.execute(query, [accountId,])
+		# query = "SELECT balance FROM finance_accounts WHERE account_id = %s"
+		# cursor.execute(query, [accountId,])
 		# remove dollar sign from result and cast as a float
-		balance = float(cursor.fetchall()[0][0].replace('$', ''))
+		balance = self.selectQuery(cursor, "finance_accounts", "account_id", [accountId,], "balance").replace('$', '')
+
+		try:
+			balance = float(balance)
+		except:	
+			balance = float(balance.replace(',',''))
 
 		# subtract cost from balance and insert into finance history table
 		newBalance = balance + income
-		updatedBalance = [accountId, newBalance, date, incomeId]
-		query = "INSERT INTO finance_account_history (account_id, balance, dt, income_id) VALUES (%s, %s, %s, %s)"
-		cursor.execute(query, updatedBalance)
+		# updatedBalance = [accountId, newBalance, date, incomeId]
+		# query = "INSERT INTO finance_account_history (account_id, balance, dt, income_id) VALUES (%s, %s, %s, %s)"
+		# cursor.execute(query, updatedBalance)
+		historyColumns = "(account_id, balance, dt, income_id)"
+		historyValues = [accountId, newBalance, date, incomeId]
+		self.insertQuery(cursor, "finance_account_history", historyColumns, historyValues)
 
 		# update account balance
 		currentBalance = [newBalance, accountId]
 		query = "UPDATE finance_accounts SET balance = %s WHERE account_id = %s"
 		cursor.execute(query, currentBalance)
+		# cursor, table, setArg, whereArg, params
+		self.updateQuery(cursor, "finance_accounts", "balance", "account_id", [newBalance, accountId])
 
 		self.closeConnection(cursor, connection)
 
@@ -152,47 +174,45 @@ class RunQuery:
 		cursor, connection = self.createConnection()
 
 		# select account id from source and receive account
-		srcIdQuery = "SELECT account_id FROM finance_accounts WHERE account = %s"
-		cursor.execute(srcIdQuery, [source,])
-		sourceId = int(cursor.fetchall()[0][0])
-
-		rcvIdQuery = "SELECT account_id FROM finance_accounts WHERE account = %s"
-		cursor.execute(rcvIdQuery, [account,])
-		receiveId = int(cursor.fetchall()[0][0])
+		sourceId = int(self.selectQuery(cursor, "finance_accounts", "account", [source,], "account_id"))
+		receiveId = int(self.selectQuery(cursor, "finance_accounts", "account", [account,], "account_id"))
 
 		# select amount from source account
-		sourceQuery = "SELECT balance FROM finance_accounts WHERE account = %s"
-		cursor.execute(sourceQuery, [source,])
-		# remove dollar sign from result and cast as a float
-		sourceBalance = float(cursor.fetchall()[0][0].replace('$', ''))
+		sourceBalance = self.selectQuery(cursor, "finance_accounts", "account", [source,], "balance").replace("$","")
+
+		try:
+			sourceBalance = float(sourceBalance)
+		except:	
+			sourceBalance = float(sourceBalance.replace(',',''))
 
 		# select amount from receiving account
-		receiveQuery = "SELECT balance FROM finance_accounts WHERE account = %s"
-		cursor.execute(receiveQuery, [account,])
-		receiveBalance = float(cursor.fetchall()[0][0].replace('$', ''))
+		receiveBalance = self.selectQuery(cursor, "finance_accounts", "account", [account,], "balance").replace("$","")
+		
+		try:
+			receiveBalance = float(receiveBalance)
+		except:	
+			receiveBalance = float(receiveBalance.replace(',',''))
 
 		# update source account with difference of balance and money
 		sourceBalance -= money
-		sourceUpdate = "UPDATE finance_accounts SET balance = %s WHERE account = %s"
-		cursor.execute(sourceUpdate, [sourceBalance, source])
+		self.updateQuery(cursor, "finance_accounts", "balance", "account", [sourceBalance, source])
 
 		# update receive account with sum of balance and money
 		receiveBalance += money
-		receiveUpdate = "UPDATE finance_accounts SET balance = %s WHERE account = %s"
-		cursor.execute(receiveUpdate, [receiveBalance, account])
+		self.updateQuery(cursor, "finance_accounts", "balance", "account", [receiveBalance, account])
 
-		# insert new value into monthly transfers table
-		insertTransfer = "INSERT INTO  transfers(amount, from_id, to_id, details, dt) VALUES (%s, %s, %s, %s, %s) RETURNING id"
-		cursor.execute(insertTransfer, [money, sourceId, receiveId, details, date])
-		transferId = int(cursor.fetchone()[0])
+		# insert new value into transfers table
+		transferColumns = "(amount, from_id, to_id, details, dt)"
+		transferValues = [money, sourceId, receiveId, details, date]
+		transferId = int(self.insertQuery(cursor, "transfers", transferColumns, transferValues, "RETURNING id"))
 
-		# update finance account history with new balance for source and receive table
-		updatedBalance = [sourceId, sourceBalance, date, transferId]
-		query = "INSERT INTO finance_account_history (account_id, balance, dt, transfer_id) VALUES (%s, %s, %s, %s)"
-		cursor.execute(query, updatedBalance)
+		# update balance in account w/ removed balance
+		historyColumns = "(account_id, balance, dt, transfer_id)"
+		historyValues = [sourceId, sourceBalance, date, transferId]
+		self.insertQuery(cursor, "finance_account_history", historyColumns, historyValues)
 
-		updatedBalance = [receiveId, receiveBalance, date, transferId]
-		query = "INSERT INTO finance_account_history (account_id, balance, dt, transfer_id) VALUES (%s, %s, %s, %s)"
-		cursor.execute(query, updatedBalance)
+		# update balance in account that received money
+		historyValues = [receiveId, receiveBalance, date, transferId]
+		self.insertQuery(cursor, "finance_account_history", historyColumns, historyValues)
 
 		self.closeConnection(cursor, connection)
--- scratch.py
+++ scratch.py
@@ -7,34 +7,14 @@ import psycopg2
 # newCsv = ImportFile('monthly_expenses.csv', 'monthly_expenses_test').csvFile()
 # print(newCsv)
 
+# vendor, cost, category, account, date, details (if any)
+newExpense = Finance().setExpense()
 
-# add expenses indidividually
-# vendor, cost, category, paymentType, date
-# newExpense = Finance().setExpense()
-
-# source, cost, category, account, details(if any), date
-newExpense = Finance('chifila', 19.9, 'fun money', 'america first credit', '2021-12-05').setExpense()
-
-# 
 # add income individually
 # source, money, category, account, date
 # newIncome = Finance().setIncome()
 
 # transfer funds
+# source, money, category, account, date, details = ""
 # newTransfer = Finance().transferFunds()
-
-
-# connection = psycopg2.connect(dbname='finances', host='localhost')
-# cursor = connection.cursor()
-
-# check = ['christmas gifts',]
-# query = "select category_id from expense_categories where expense_category = %s"
-# cursor.execute(query, check)
-
-# results = cursor.fetchall()
-# connection.commit()
-# cursor.close()
-# connection.close()
-
-# print(len(results))
 	
